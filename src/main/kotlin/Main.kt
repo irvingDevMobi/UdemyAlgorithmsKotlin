ffbd75d3c0714c660e51e61b6c363756d341eabf
Irving Lop <irving.dev.mobi@gmail.com>
2024-01-03 12:43:19 -0600

    Sort - Selection Sort

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,13 +1,15 @@
 package dev.irving
 
 import dev.irving.sort.BubbleSort
+import dev.irving.sort.SelectionSort
 
 //TIP Press <shortcut raw="SHIFT"/> twice to open the Search Everywhere dialog and type <b>show whitespaces</b>,
 // then press <shortcut raw="ENTER"/>. You can now see whitespace characters in your code.
 fun main() {
     val array = intArrayOf(4, 6, 54, -45, 9, 18)
     array.forEach { print("$it ") }
-    BubbleSort(array)
+    //BubbleSort(array)
+    SelectionSort(array)
     println()
     array.forEach { print("$it ") }
 }
